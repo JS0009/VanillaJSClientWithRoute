a9bd49e6eb1dd90ff7c41258991281021f28e92c
maga_bash <caitov.magomed@yandex.ru>
2023-05-17 18:11:17 +0300

    create-modele

--- index.js
+++ index.js
@@ -1,44 +1,4 @@
-class HistoryRouter {
-    constructor() {
-        this.routes = {};
-        this.currentUrl = '';
-    }
-
-    route(path, callback) {
-        this.routes[path] = callback;
-    }
-
-    updateView() {
-        const url = location.pathname;
-        if (this.currentUrl === url) {
-            return;
-        }
-
-        if (!this.routes[url]) {
-            console.error(`No route defined for ${url}`);
-            return;
-        }
-
-        document.querySelector('div').innerHTML = this.routes[url]();
-        this.currentUrl = url;
-    }
-
-    init() {
-        window.addEventListener('popstate', this.updateView.bind(this));
-
-        const links = document.querySelectorAll('nav a');
-        links.forEach((link) => {
-            link.addEventListener('click', (event) => {
-                event.preventDefault();
-                const path = link.getAttribute('href');
-                history.pushState(null, null, path);
-                this.updateView();
-            });
-        });
-
-        this.updateView();
-    }
-}
+import HistoryRouter from "./router";
 
 const router = new HistoryRouter();
 
--- router.js
+++ router.js
@@ -1,73 +1,42 @@
-// Создаём класс Router
-class Router {
-    // Конструктор получает массив маршрутов и запускает инициализацию.
-    constructor(routes) {
-        this.routes = routes;
-        this._loadInitialRoute();
+export default class HistoryRouter {
+    constructor() {
+        this.routes = {};
+        this.currentUrl = '';
     }
 
-    // Загрузка начального маршрута при перезагрузке страницы или при первом посещении.
-    _loadInitialRoute() {
-        // Определение параметров в URL и преобразование URL в массив.
-        const pathNameSplit = window.location.pathname.split('/');
-        const pathParams = pathNameSplit.slice(2);
-
-        // Определение пути в URL и преобразование пути в массив.
-        const pathSegments = this._getPathSegments(window.location.hash);
-        // Находим маршрут, который соответствует пути.
-        const matchedRoute = this._matchUrlToRoute(pathSegments, pathParams);
-
-        // Загружаем компонент, связанный с найденным маршрутом.
-        this._loadComponent(matchedRoute.component);
+    route(path, callback) {
+        this.routes[path] = callback;
     }
 
-    // Сравниваем путь и параметры в URL с каждым маршрутом в массиве маршрутов и находим маршрут, который соответствует URL.
-    _matchUrlToRoute(pathSegments, pathParams) {
-        const matchedRoute = this.routes.find(route => {
-            // Разбираем путь маршрута на массив и сравниваем количество элементов массива маршрута с количеством элементов массива пути.
-            const routePathSplit = route.path.split('/');
-            if (routePathSplit.length !== pathSegments.length) {
-                return false;
-            }
-            // Проверяем каждый элемент массива маршрута и пути на соответствие.
-            const match = routePathSplit.every((routePathSegment, i) => {
-                return routePathSegment === pathSegments[i] || routePathSegment[0] === ':';
+    updateView() {
+        const url = location.pathname;
+        if (this.currentUrl === url) {
+            return;
+        }
+
+        if (!this.routes[url]) {
+            console.error(`No route defined for ${url}`);
+            return;
+        }
+
+        document.querySelector('div').innerHTML = this.routes[url]();
+        this.currentUrl = url;
+    }
+
+    init() {
+        window.addEventListener('popstate', this.updateView.bind(this));
+
+        const links = document.querySelectorAll('nav a');
+        links.forEach((link) => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault();
+                const path = link.getAttribute('href');
+                history.pushState(null, null, path);
+                this.updateView();
             });
-            // Если маршрут соответствует URL, добавляем параметры в массив параметров.
-            if (match) {
-                for (let i = 0; i < routePathSplit.length; i++) {
-                    if (routePathSplit[i][0] === ':') {
-                        pathParams.push(pathSegments[i]);
-                    }
-                }
-            }
-            // Возвращаем true, если маршрут соответствует URL.
-            return match;
         });
 
-        return matchedRoute;
+        this.updateView();
     }
+}
 
-    // Переводим хеш URL в массив пути.
-    _getPathSegments(hash) {
-        return hash.split('/').map(segment => {
-            return segment.replace(/^#/, '');
-        });
-    }
-
-    // Загружаем компонент, связанный с маршрутом.
-    _loadComponent(componentName, pathParams = []) {
-        // Извлекаем имя модуля компонента из имени файла.
-        const module = componentName.split('_')[0];
-
-        // Импортируем модуль компонента.
-        import(`./${module}.js`).then(module => {
-            // Получаем функцию, которая отрисовывает компонент и передаём ей массив параметров.
-            const page = module.default;
-            page(pathParams);
-        }).catch(err => {
-            console.error(`Error loading component: ${err}`);
-        });
-    }
-
-  // Метод для изменения хеш URL в браузере и вызова _
