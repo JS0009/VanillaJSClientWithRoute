67fc0c88f3376f32dd72529bd9e5d184b343c85d
maga_bash <caitov.magomed@yandex.ru>
2024-05-07 13:03:20 +0300

    comment

--- src/app/pokemon.js
+++ src/app/pokemon.js
@@ -2,28 +2,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 
-
-// Используйте функцию dispatch для отправки действий
-//store.dispatch({ type: 'ACTION_TYPE', payload: 'Some payload' });
-
-
 const api = createApi({
     reducerPath: 'api',
-    //baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
     baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
     endpoints: (builder) => ({
-        // Define your endpoints here
-        // Example:
         getUsers: builder.query({
             query: () => 'berry',
         }),
     }),
 });
 
-// Создайте хранилище Redux
 const rootReducer = combineReducers({
     [api.reducerPath]: api.reducer,
-    // other reducers...
 });
 
 const store = configureStore({
@@ -33,14 +23,9 @@ const store = configureStore({
 });
 const { dispatch } = store
 
-// const { useGetUsersQuery } = api;
-
-// Usage example
 export const getUsers = async () => {
     const response = dispatch(api.endpoints.getUsers.initiate());
-    //const promise = dispatch(api.endpoints.getPosts.initiate())
-    //const { refetch } = promise
-    // interact with the cache in the same way as you would with a useFetch...() hook
+
     const { data, isLoading, isSuccess, error } = await response
 
     if (error) {
@@ -51,21 +36,3 @@ export const getUsers = async () => {
     console.log('Users:', data);
 };
 
-//getUsers();
-
-
-// import { createApi, fetchBaseQuery, dispatch } from '@reduxjs/toolkit/query'
-// //import type { Pokemon } from './types'
-
-// // Define a service using a base URL and expected endpoints
-// export const pokemonApi = createApi({
-//     reducerPath: 'pokemonApi',
-//     baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
-//     endpoints: (builder) => ({
-//         getPokemonByName: builder.query({
-//             query: (name) => `pokemon/${name}`,
-//         }),
-//     }),
-// })
-
-// export default dispatch(pokemonApi.endpoints.getPokemonByName.initiate())
