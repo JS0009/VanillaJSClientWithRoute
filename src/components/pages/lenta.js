645bd330f32818801c85d7b44ca5bb4b52ff0810
maga_bash <caitov.magomed@yandex.ru>
2024-05-19 12:54:16 +0300

    cards2

--- src/components/pages/lenta.js
+++ src/components/pages/lenta.js
@@ -11,6 +11,11 @@ export default class Lenta extends Page {
             const lenta = document.getElementById("card"); // создаем новый элемент div
             const cards = lenta.cloneNode(true);
             cards.querySelector("h5").textContent = card.name; // задаем текст для нового элемента
+            cards.querySelector("#category").textContent = card.category; // задаем текст для нового элемента
+            cards.querySelector("#description").textContent = card.description; // задаем текст для нового элемента
+            cards.querySelector("#price").textContent = card.price; // задаем текст для нового элемента
+            cards.querySelector("#brand").textContent = card.brand; // задаем текст для нового элемента
+            cards.querySelector("#rating").textContent = card.rating; // задаем текст для нового элемента
             console.log(cards);
             this.parent.appendChild(cards); // добавляем новый элемент в конец списка дочерних элементов родительского элемента
         });
